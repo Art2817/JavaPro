8304380b593793ae6495ebba6bb477f34da3e3dc
Artem <artem.cherednychenko2817@gmail.com>
2026-01-15 11:27:12 +0100

    Add dz7

---
+++ src/dz7/Main.java
@@ -0,0 +1,15 @@
+package dz7;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        getOutput(new Corrector().handleData(
+                new DataProvider().getData())
+        );
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
+    }
+}
