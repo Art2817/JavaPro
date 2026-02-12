de6f64ad325c420b75114fe7df6f0cda68410467
Artem <artem.cherednychenko2817@gmail.com>
2026-02-12 13:07:51 +0100

    Add dz15

---
+++ src/dz15/DataHandler.java
@@ -0,0 +1,31 @@
+package dz15;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class DataHandler {
+
+   private final Map<Integer, String> map;
+
+   public DataHandler() {
+       map = new DataRepository().getData();
+   }
+
+    public String getAll() {
+        StringBuilder sb = new StringBuilder();
+        AtomicInteger count = new AtomicInteger(0);
+        map.forEach((id, name) ->
+                sb.append(String.format("%d) %d, %s%n",
+                        count.incrementAndGet(),id, name)
+        ));
+        return "\nALL NAMES:\n" + sb;
+    }
+
+
+    public String getById(int id) {
+        if (map.containsKey(id)) {
+            return "\nNAME: id " + id + ", " +
+                    map.get(id);
+        } else return "No data!";
+    }
+}
---
+++ src/dz15/UIOperator.java
@@ -0,0 +1,8 @@
+package dz15;
+
+public class UIOperator {
+
+    public void getOutput(String output) {
+        System.out.println(output);
+    }
+}
